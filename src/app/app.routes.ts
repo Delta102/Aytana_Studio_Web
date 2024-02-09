8a65f440ca98833c997c9797010f1b1ffb9b1639
Felix Peralta <alfrefp10001@gmail.com>
2024-02-09 14:24:30 -0500

    Act route for homepage, Act template for homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { AboutMeComponent } from './components/home/about-me/about-me.component';
+import { HomeComponent } from './components/home/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'about-me',
     component: AboutMeComponent
   },
+
 ];
